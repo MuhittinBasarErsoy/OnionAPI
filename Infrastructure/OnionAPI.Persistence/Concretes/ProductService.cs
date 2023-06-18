8542d805857c28d41e9862e920ae41a842fe855b
BasarErsoy <basarersoy@gmail.com>
2023-06-18 17:18:10 +0300

    Add project files.

---
+++ Infrastructure/OnionAPI.Persistence/Concretes/ProductService.cs
@@ -0,0 +1,28 @@
+﻿using OnionAPI.Application.Abstraction;
+using OnionAPI.Domain.Entities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace OnionAPI.Persistence.Concretes
+{
+    internal class ProductService : IProductService
+    {
+        public List<Product> GetProducts()
+        {
+            List<Product> products = new List<Product>
+            {
+             new Product { Price = 1, Stock = 1, Name = "bir", CreateDate = DateTime.Now, Id = new Guid()},
+             new Product { Price = 1, Stock = 1, Name = "iki", CreateDate = DateTime.Now, Id = new Guid()}
+            };
+
+        
+            return products;
+        }
+    }
+}
